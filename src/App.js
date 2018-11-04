70cf9433b5875f6a44fc853b7b23af1f49d49953
zachjustice <zachjustice123@gmail.com>
2018-11-04 18:58:27 -0500

    Fix issue where options stayed highlighted after going back selecting another choice

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import './App.css';
 import {Storyboard} from './components/Storyboard';
 import {ChoicesService, INITIAL_CHOICE_ID} from './services/Choices.service';
+import { cloneDeep } from 'lodash';
 
 
 class App extends Component {
     choicesService = new ChoicesService();
-    state = { choices: [ this.choicesService.getChoice(INITIAL_CHOICE_ID) ] };
+    state = { choices: [ cloneDeep(this.choicesService.getChoice(INITIAL_CHOICE_ID)) ] };
 
     choose = (parentChoice, selectedOption, index) => {
         this.setState((state) => {
@@ -14,8 +15,10 @@ class App extends Component {
                 option.isSelected = option.id === selectedOption.id;
             });
 
-            const choices = state.choices.slice(0, index + 1).concat(this.choicesService.getChoice(selectedOption.next));
-            console.log('    new choices', choices);
+            const choices = state.choices
+                .slice(0, index + 1)
+                .concat(cloneDeep(this.choicesService.getChoice(selectedOption.next)));
+
             return {
                 ...state,
                 choices,
